f7ff011cf097a1ceae0958b8f5f8a083766ef302
Made Budhi <122160601+Made-Budhi@users.noreply.github.com>
2023-06-13 17:05:58 +0800

    Initial commit

---
+++ Game/Turn.java
@@ -0,0 +1,15 @@
+package NewTictactoe.Game;
+
+public class Turn {
+
+    private int indexTurn = 1;
+
+    public int getIndexTurn() {
+        return indexTurn;
+    }
+
+    public void setIndexTurn(int indexTurn) {
+        this.indexTurn = indexTurn;
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+package NewTictactoe;
+
+import NewTictactoe.Frame.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new MenuFrame();
+    }
+}
